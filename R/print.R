cec9739ae8bf0936b047a4f2c722ba3d14744f75
John Coene <jcoenep@gmail.com>
2019-07-29 17:38:35 +0200

    better print

--- R/print.R
+++ R/print.R
@@ -5,6 +5,22 @@ print.webhoserToken <- function(x, ...) {
 
 #' @export
 print.webhoser <- function(x, ...) {
-  hidden <- attr(x, "hidden")
-  print(x[!names(x) %in% hidden], ...)
+  warn <- crayon::green
+  sym <- cli::symbol$tick
+  if(x$requestsLeft < 600){
+    sym <- cli::symbol$warning
+    color <- crayon::yellow
+  }
+  if(x$requestsLeft < 250){
+    sym <- cli::symbol$cross
+    color <- crayon::red
+  }
+
+  cat(warn(sym), x$requestsLeft, "Requests left\n")
+
+  if(length(x$totalResults))
+    cat(cli::symbol$pointer, crayon::blue(x$totalResults), "Articles collected\n")
+
+  if(length(x$moreResultsAvailable))
+    cat(cli::symbol$pointer, crayon::blue(x$moreResultsAvailable), "Articles still available\n")
 }
